6ac0a09f5de7dfaed18b630c5aeb2bd992a89d82
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-10-23 01:42:10 -0400

    update add `reset` result area

--- Labwork-4/script.js
+++ Labwork-4/script.js
@@ -3,22 +3,30 @@
 /* Circle Area */
 function circleArea() {
   console.info("Calculate Circle Area")
+  /* Reset result area */
+  document.getElementById("circle-area").value = "NaN"
 }
 
 
 /* Triangle Area */
 function triangleArea() {
   console.info("Calculate Triangle Area")
+  /* Reset result area */
+  document.getElementById("triangle-area").value = "NaN"
 }
 
 
 /* Square Area */
 function squareArea() {
   console.info("Calculate Square Area")
+  /* Reset result area */
+  document.getElementById("square-area").value = "NaN"
 }
 
 
 /* Rectangle Area */
 function rectangleArea() {
   console.info("Calculate Rectangle Area")
+  /* Reset result area */
+  document.getElementById("rectangle-area").value = "NaN"
 }
