45ceb5271fbe1ff38fc81ffa1eaace2567bbd43b
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-10-23 01:41:17 -0400

    update add `console.info()`

--- Labwork-4/script.js
+++ Labwork-4/script.js
@@ -2,19 +2,23 @@
 
 /* Circle Area */
 function circleArea() {
+  console.info("Calculate Circle Area")
 }
 
 
 /* Triangle Area */
 function triangleArea() {
+  console.info("Calculate Triangle Area")
 }
 
 
 /* Square Area */
 function squareArea() {
+  console.info("Calculate Square Area")
 }
 
 
 /* Rectangle Area */
 function rectangleArea() {
+  console.info("Calculate Rectangle Area")
 }
