c7c58ca7119c42ae22908677f216c302008e48a5
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-11-04 20:00:08 -0400

    update `userForm()`: Get values from inputs

--- Labwork-5/js/script.js
+++ Labwork-5/js/script.js
@@ -5,6 +5,23 @@
 
 function userForm() {
   console.info("Submit - Home")
+  
+  //? Get values from inputs
+  let fname = document.getElementById("fname").value
+  let lname = document.getElementById("lname").value
+  let email = document.getElementById("email").value
+  let address = document.getElementById("address").value
+  let city = document.getElementById("city").value
+  let province = document.getElementById("province").value
+  let country = document.getElementById("country").value
+  let membership
+  if (document.getElementById("premium-membership").checked) {
+    membership = document.getElementById("premium-membership").value
+  } else if (document.getElementById("standard-membership").checked) {
+    membership = document.getElementById("standard-membership").value
+  } else if (document.getElementById("basic-membership").checked) {
+    membership = document.getElementById("basic-membership").value
+  }
 }
 
 
