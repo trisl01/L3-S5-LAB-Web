e0db796c6275ee21e28225855b63ff4ccd5b54b1
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-11-11 22:38:24 -0500

    update add `refresh` if you go back

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    //window.location.reload()
+    next()
+  }
+})
+
 let newLab = "L5"
 
 let dataPath
