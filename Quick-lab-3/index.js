d6e7adbc0d9d8db809ab4d6a95aabb3ffe3a88ae
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-11-23 00:40:07 -0500

    update add `Setup` part

--- Quick-lab-3/index.js
+++ Quick-lab-3/index.js
@@ -1,1 +1,18 @@
+//! ---------------------------------------------
+//?  Setup
+//! ---------------------------------------------
 
+//? Open Thunder Client on VSCode to call the requests
+
+import express from "express";
+import data from './data/mock-data.json' assert { type: 'json' };
+
+const app = express();
+
+const PORT = 3000;
+
+//? Display in the console
+app.listen(PORT, () => {
+    console.log(`Express App Server listening on port ${PORT} and the local server URL: http://localhost:3000/`);
+    //console.log(data);
+});
