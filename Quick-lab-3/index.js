6e6c665f753e05601f375128499dea9f03ce3968
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-11-23 00:40:29 -0500

    update add `GET Requests` part

--- Quick-lab-3/index.js
+++ Quick-lab-3/index.js
@@ -16,3 +16,50 @@ app.listen(PORT, () => {
     console.log(`Express App Server listening on port ${PORT} and the local server URL: http://localhost:3000/`);
     //console.log(data);
 });
+
+
+//! ---------------------------------------------
+//?  GET Requests
+//! ---------------------------------------------
+
+// URL => http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('<h1>ExpressJS App: The root</h1>');
+});
+
+// URL => http://localhost:3000/about
+app.get('/about', (req, res) => {
+    res.send('<h1>About Express</h1>');
+});
+
+// URL => http://localhost:3000/users
+app.get("/users", (req, res) => {
+    res.json(data);
+});
+// URL => http://localhost:3000/users/34
+app.get("/users/:userId", (req, res) => {
+    console.log(req.params);
+    res.send("User ID: " + req.params.userId);
+});
+
+// http://localhost:3000/members/28/age/47
+app.get("/members/:memberId/age/:age", (req, res) => {
+    console.log(req.params);
+    const memberId = Number(req.params.memberId);
+    const memberAge = Number(req.params.age);
+    res.send(`Member ID: ${memberId} <br> Member Age: ${memberAge}`);
+});
+// http://localhost:3000/employees/12
+app.get("/employees/:id", (req, res) => {
+    const empId = Number(req.params.id);
+    const employee = data.filter((employee) => {
+        return employee.id === empId;
+    });
+    res.send(employee);
+});
+// http://localhost:3000/workers/12
+app.get("/workers/:id/", (req, res) => {
+    const workerId = Number(req.params.id);
+    const worker = data.filter((element) => element.id === workerId);
+    res.send(worker);
+});
